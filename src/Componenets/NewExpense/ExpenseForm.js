ccd740827a306e033c1c912b3b4069e28ea25274
Ankit2708 <69914264+Ankit2708@users.noreply.github.com>
2023-04-06 23:49:12 +0530

    task6commit

--- src/Componenets/NewExpense/ExpenseForm.js
+++ src/Componenets/NewExpense/ExpenseForm.js
@@ -4,17 +4,47 @@ function ExpenseForm(){
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
+    // const [userInput, setUserInput]=useState({
+    //     enteredTitle:"",
+    //     enteredAmount:"",
+    //     enteredDate:""
+    // })
     const titleChangeHandler =(event)=>{
         setEnteredTitle(event.target.value)
+    //     setUserInput({
+    //     ...userInput,
+    //     enteredTitle:event.target.value
+
+    // })
+    // setUserInput((prevState)=>{
+    //     return {...prevState, enteredTitle:event.target.value}  // this is more correct way of using songle state
+    // })
     }
     const amountChangeHandler = (event)=>{
         setEnteredAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:event.target.value
+        // })
     }
     const dateChangeHandler =(event)=>{
         setEnteredDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate:event.target.value
+        // })
+    }
+    const formSubmitHandler = (event)=>{
+        event.preventDefault()
+        const values ={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate)
+        }
+        console.log(values)
     }
     return (
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
